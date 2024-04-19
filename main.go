fe1c87a046444a49209765ac0faf650fb0dfc3fd
Hajizada Farid <faridmh@code.edu.az>
2024-04-19 11:06:24 +0400

    feat: add url

--- main.go
+++ main.go
@@ -5,12 +5,29 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Home Page!")
+}
 
+func contact_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Contact Page !")
+}
+
+func about_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the About Page !")
+}
+
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contact", contact_page)
+	http.HandleFunc("/about", about_page)	
+	
 	http.ListenAndServe(":8080", nil)
-	fmt.Println("Server is running on port 8080")
+}
+
+
+func main() {
+	handleRequest()
 }
 
