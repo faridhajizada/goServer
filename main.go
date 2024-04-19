d3af06c894d7f973ed4eaee7f7919793cbe326a0
Hajizada Farid <faridmh@code.edu.az>
2024-04-19 11:39:34 +0400

    feat: add struct

--- main.go
+++ main.go
@@ -5,29 +5,46 @@ import (
 	"net/http"
 )
 
-func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Home Page!")
+type User struct {
+	name     string
+	age      uint16
+	money    int16
+	avgGrades, happiness float64
+	hobbies  []string
 }
 
-func contact_page(w http.ResponseWriter, r *http.Request) {
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s age is: %d money is: %d average grades is: %.2f happiness is: %.2f hobbies are: %v", u.name, u.age, u.money, u.avgGrades, u.happiness, u.hobbies)
+}
+
+func (u *User) setNewName(newName string) {
+	u.name = newName
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Bob", 25, 1000, 4.2, 0.8, []string{"Football", "Guitar"}}
+	bob.setNewName("Alex")
+	fmt.Fprintf(w, bob.getAllInfo())
+	
+}
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Contact Page !")
 }
 
-func about_page(w http.ResponseWriter, r *http.Request) {
+func aboutPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the About Page !")
 }
 
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/contact", contactPage)
+	http.HandleFunc("/about", aboutPage)
 
-func handleRequest() {
-	http.HandleFunc("/", home_page)
-	http.HandleFunc("/contact", contact_page)
-	http.HandleFunc("/about", about_page)	
-	
+	fmt.Println("Server is running on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
 
-
 func main() {
-	handleRequest()
+	handleRequests()
 }
-
